fc9ad60534a5b78bbc4d667659456fefb9a30037
yousuf-safin <yousufalisafin@gmail.com>
2024-09-11 13:28:03 +0600

    Model

--- app/Models/job.php
+++ app/Models/job.php
@@ -1,35 +1,10 @@
 <?php
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array {
-        return [
-            [   
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$10,000'  
-            ],
-            [   
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$20,000'  
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$60,000'  
-            ]          
-        ];
-    }
+class Job extends Model{
+   protected $table = 'job_listings';
 
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job; // Make sure to return the found job
-    }
+   protected $fillable = ['title', 'salary'];
 }
